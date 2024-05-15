b3f2462b58c8b5c613943861c0fb0f8361ef73c8
bradzara <bradzara93@gmail.com>
2024-05-14 23:58:19 -0700

    Define filter method

--- bored_api.rb
+++ bored_api.rb
@@ -14,7 +14,56 @@ def random
 end
 
 def filter
-  puts "This is the filter method"
+  puts "Choose a type of activity:"
+  puts "education | recreational | social | diy | charity | cooking | relaxation | music | busywork"
+  type = gets.chomp
+  puts "How many total people will be participating in the activity?"
+  participants = gets.chomp.to_i
+  puts "On a decimal scale between 0 and 1, how much do you want the activity to cost? 0 being free and 1 being the most expensive:"
+  price = gets.chomp.to_f
+  puts "On a decimal scale between 0 and 1, how accessible do you want this event to be? 0 being the most accessible and 1 being the least accessible:"
+  accessibility = gets.chomp.to_f
+
+  # Validate input
+  valid_types = ["education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"]
+  type = type.downcase
+  unless valid_types.include?(type)
+    puts "Invalid activity type."
+    return
+  end
+
+  if participants <= 0
+    puts "Participants should be a positive number."
+    return
+  end
+
+  if price < 0 || price > 1
+    puts "Price should be between 0 and 1."
+    return
+  end
+
+  if accessibility < 0 || accessibility > 1
+    puts "Accessibility should be between 0 and 1."
+    return
+  end
+
+  # Build the query string
+  query_params = {
+    type: type,
+    participants: participants,
+    price: price,
+    accessibility: accessibility
+  }.map { |k, v| "#{k}=#{v}" }.join("&")
+
+  url = "http://www.boredapi.com/api/activity?#{query_params}"
+  response = HTTP.get(url).to_s
+  data = JSON.parse(response)
+
+  if data['error']
+    puts data['error']
+  else
+    puts data['activity']
+  end
 end
 
 def menu
