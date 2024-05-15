669e0d2daec3f9add259bc9c2441e6cd567bbd3f
bradzara <bradzara93@gmail.com>
2024-05-14 18:10:37 -0700

    Define intial methods

--- bored_api.rb
+++ bored_api.rb
@@ -2,4 +2,27 @@ require "http"
 
 random = HTTP.get("http://www.boredapi.com/api/activity/").parse
 
-p random
+def random
+  puts "This is the random choice method"
+end
+
+def filter
+  puts "This is the filter method"
+end
+
+def menu
+  puts "Press 1 for a random choice, or press 2 to filter your choices:"
+  user_input = gets.chomp.to_i
+
+  if user_input == 1
+    return random
+  elsif user_input == 2
+    return filter
+  else
+    puts "Invalid option"
+    return menu
+  end
+end  
+
+menu
+
